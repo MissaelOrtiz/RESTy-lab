d1a2b4bebce795bddf90d9ce7a9e2348136712b5
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-13 10:30:51 -0700

    added event handlers to container

--- src/containers/RestyContainer.jsx
+++ src/containers/RestyContainer.jsx
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 
 export default class RestyContainer extends Component {
   state = {
-    URL: '',
+    url: '',
     method: '',
     rawJson: '',
     res: ''
   }
+
+  handleURLChange = (event) => {
+    this.setState({ url: event.target.value });
+  }
+
+  handleMethodChange = (event) => {
+    this.setState({ method: event.target.value });
+  }
+
+  handleRawJsonChange = (event) => {
+    this.setState({ rawJson: event.target.value });
+  }
+
+  handleRes = (res) => {
+    this.setState({ res });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
