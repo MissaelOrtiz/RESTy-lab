18e262adf5ed25df645af87358ce40f2e5ac7983
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-13 11:02:23 -0700

    added Header and slight css

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import RestyContainer from '../../containers/RestyContainer';
+import Header from '../RESTy/Header';
 
 export default function App() {
-  return <RestyContainer />;
+  return <>
+    <Header />
+    <RestyContainer />
+  </>;
 }
