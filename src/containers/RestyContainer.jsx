9109546ea55be6f9e457d9c29021935332646e33
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-13 10:18:01 -0700

    added inital file strcture

---
+++ src/containers/RestyContainer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class RestyContainer extends Component {
+  state = {
+    URL: '',
+    method: '',
+    rawJson: '',
+    res: ''
+  }
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    );
+  }
+}
